4f61a1ea15f84bfb2543ef1ece9f414fa9abdf8f
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-10-29 15:50:12 +0900

    stateの基本的な使い方

--- src/App.js
+++ src/App.js
@@ -3,13 +3,53 @@ import Rect from './Rect';
 import './App.css';
 
 class App extends Component {
+  msgStyle = {
+    fontSize: "24pt",
+    color: "#900",
+    margin: "20px 0px",
+    padding: "5px",
+    borderBottom: "2px solid #900"
+  }
+  msgStyle2 = {
+    fontSize: "20pt",
+    color: "#fff",
+    backgroundColor: "#900",
+    margin: "20px 0px",
+    padding: "5px",
+    borderBottom: "2px solid #900"
+  }
+  btnStyle = {
+    fontSize: "16pt",
+    padding: "10px"
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+      msg: 'count start!',
+      flg: true,
+    };
+    this.doAction = this.doAction.bind(this);
+  }
+
+  doAction(e) {
+    this.setState((state) => ({
+      counter: state.counter + 1,
+      msg: 'count:' + state.counter,
+      flg: !state.flg
+    }));
+  }
   render() {
     return (
       <div>
         <h1>React</h1>
-        <Rect x="50" y="50" w="150" h="150" c="#6ff9" r="50" />
-        <Rect x="150" y="100" w="150" h="150" c="#f6f9" r="75" />
-        <Rect x="100" y="150" w="150" h="150" c="#6669" r="25" />
+        {this.state.flg ?
+          <p style={this.msgStyle}>{this.state.msg}</p>
+          :
+          <p style={this.msgStyle2}>{this.state.msg}です</p>
+        }
+          <button style={this.btnStyle} onClick={this.doAction}>Click</button>
       </div>
     )
   }
